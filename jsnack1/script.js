44a158e78b485a695dc35697af6f9d01743a2291
Maria Stachie <mary.stachie@gmail.com>
2022-03-10 16:10:36 +0100

    Init

---
+++ jsnack1/script.js
@@ -0,0 +1,7 @@
+console.log('OK JS')
+
+// The Great Gatsby party !
+// chiedi all'utente il suo nome e
+// comunicagli se può partecipare alla festa
+
+const guests = ['Scott Fitzgerald', 'Leonardo DiCaprio', 'Daisy Fay', 'Nick Carraway', 'Mary Stachie'];
